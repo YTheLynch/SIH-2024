c16022f39cf8b78b9864883a2b871752719a6bd7
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2024-09-11 01:49:34 +0530

    Add Search Functionality

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,21 +1,44 @@
 "use client"
-import React from "react"
-import SearchItem from "@/components/SearchItem"
-import { schemes } from "@/store/schemes"
-import { Navbar } from "@/components/Navbar"
+import React from 'react'
+import { Navbar } from '@/components/Navbar'
+import SearchItem from '@/components/SearchItem'
+import { schemes } from '@/store/schemes'
+import SearchBar from '@/components/SearchBar'
+import { useState } from 'react'
 
-const page = () => {
+
+const Page = () => {
+  const [searchq, setSearchq] = useState('');
+
+
+  const handleChange = (e) => {
+      setSearchq(e.target.value)
+  }
+
+   const genResults = () => {
+    return searchq === '' ? (
+      schemes.map((scheme, index) => (
+        <SearchItem key={index} name={scheme.name} organization={scheme.organization.join(", ")} className="m-6" />
+      ))
+    ) : (
+      schemes.filter(scheme => scheme.name.toLowerCase().includes(searchq.toLowerCase()) || scheme.organization.join(", ").toLowerCase().includes(searchq.toLowerCase())).map((scheme, index) => (
+        <SearchItem key={index} name={scheme.name} organization={scheme.organization.join(", ")} className="m-6" />
+      ))
+    );
+   }
+
   return (
     <>
-      <Navbar />
-      <div id="box" className="ml-20 pr-20 min-h-[70vh]">
-        {schemes.map((scheme, index) => (
-          <SearchItem key={index} name={scheme.name} organization={scheme.organization.join(", ")} className="m-6" />
-        ))}
-      </div>
-    </>
-  );
-};
+    
+    <Navbar hideSearch = {true}/>
+    <SearchBar handleChange = {handleChange}/>
 
-export default page;
+    <div id="box" className="ml-20 pr-20 min-h-[70vh]">
+      {genResults()}  
+    </div>
+    
+  </>
+  )
+}
 
+export default Page

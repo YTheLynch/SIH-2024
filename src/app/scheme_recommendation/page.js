87952a118219277c5f82389449e87f09b68d7b5e
aman4374 <amantrivedi4374@gmail.com>
2024-09-11 01:30:07 +0530

    UI for model, Update blog

---
+++ src/app/scheme_recommendation/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import FormComponent from '@/components/FormComponent';
+
+const SchemeRecommendationPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-red-100 to-red-200">
+      <FormComponent />
+    </div>
+  );
+};
+
+export default SchemeRecommendationPage;

610b0175927e8f5c67d1ae9a3f9bdb1882312a6f
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2024-09-11 10:24:45 +0530

    Update searchItem component

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -3,8 +3,8 @@ import React from 'react'
 const SearchItem = (props) => {
   return (
     <div className={`bg-white text-black rounded-md min-h-8 hover:text-red-700 hover:scale-105 hover:transition-scale  p-8 text-bold border border-solid shadow-md border-gray-900 ${props?.className}`}>
-        <p className='mb-5 text-[1.rem]'>{props?.name}</p>
-        <p className='text-[1rem]'>{props?.organization}</p>
+        <p className='mb-5 text-[1rem]'>{props?.name}</p>
+        <p className='text-[0.8rem]'>{props?.organization}</p>
     </div>
   )
 }
